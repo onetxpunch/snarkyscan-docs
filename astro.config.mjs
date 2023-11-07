6fc07a50af25fd05a5e3a39918984c93fbe119e4
punch <117326984+onetxpunch@users.noreply.github.com>
2023-11-07 05:06:41 -0500

    init from docs

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/noop",
+    },
+  },
+  base: "/astro-starlight-docs/",
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
